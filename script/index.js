0c68450f4cffeea97e1a11247bf30be6b91cd0e7
Alena Panasova <alena.pana5ova@yandex.ru>
2020-09-04 22:53:17 +0300

    Внести правки после ревью.

--- script/index.js
+++ script/index.js
@@ -5,46 +5,55 @@ const popupOverlay = document.querySelector('.popup');
 const profileTitle = document.querySelector('.profile-info__title');
 const profileSubtitle = document.querySelector('.profile-info__subtitle');
 
-//обращаемся к инпутам попапа
-const popupInputTitle = document.querySelector('input[name="firstname"]');
-const popupInputSubtitle = document.querySelector('input[name="vocation"]');
+//обращаемся к форме и инпутам попапа
+const form = document.getElementById('input-form');
+const popupInputTitle = form.firstname;
+const popupInputSubtitle = form.vocation;
 
+//обращаемся к кнопке редактирования попапа
+const profileEditButton = document.querySelector('.profile-info__edit-button');
+
+//обращаемся к кнопке закрытия попапа
+const closeIcon = document.querySelector('.popup__close-icon');
+
+
+function closePopup() {
+    popupOverlay.classList.remove('popup_opened');
+}
 
 //закрываем попап
-const closeIcon = document.querySelector('.popup__close-icon');
-closeIcon.onclick = function () {
-    popupOverlay.classList.remove("popup_opened");
-}
+//добавлен слушатель
+closeIcon.addEventListener('click', function () {
+    closePopup();
+})
 
 //открываем попап
-const profileEditButton = document.querySelector('.profile-info__edit-button');
-profileEditButton.onclick = function () {
-    popupOverlay.classList.add("popup_opened");
+//добавляем слушатель
+profileEditButton.addEventListener('click', function () {
+    popupOverlay.classList.add('popup_opened');
+})
 
-    //проверяем не пустые ли значения и заполняем поля попапа
-    if (profileTitle.innerHTML !== "") {
-        popupInputTitle.value = profileTitle.innerHTML;
-    }
-    if (profileSubtitle.innerHTML !== "") {
-        popupInputSubtitle.value = profileSubtitle.innerHTML;
-    }
+//заполняем поля попапа
+if (profileTitle.textContent) {
+    popupInputTitle.value = profileTitle.textContent;
+}
+if (profileSubtitle.textContent) {
+    popupInputSubtitle.value = profileSubtitle.textContent;
 }
 
-//поймать отправку формы
-const form = document.getElementById('input-form');
-form.onsubmit = function (event) {
+//функция отправки формы
+form.addEventListener('submit', function (event) {
 
     //отправляем действие по умолчанию
     event.preventDefault();
 
     //сохраняем значение полей
-    profileTitle.innerHTML = popupInputTitle.value;
-    profileSubtitle.innerHTML = popupInputSubtitle.value;
+    profileTitle.textContent = popupInputTitle.value;
+    profileSubtitle.textContent = popupInputSubtitle.value;
 
     //закрываем попап
-    popupOverlay.classList.remove("popup_opened");
-}
-
+    closePopup();
+})
 
 
 
